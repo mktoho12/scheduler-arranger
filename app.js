76e9fc33f6ffb5bc074dc906877f8b5877985271
Makoto Ishida <makoto_16n1200102@nnn.ed.jp>
2018-01-12 03:36:01 +0000

    GitHubの認証を実装しました

--- app.js
+++ app.js
@@ -1,60 +1,102 @@
-var express = require('express');
-var path = require('path');
-var favicon = require('serve-favicon');
-var logger = require('morgan');
-var cookieParser = require('cookie-parser');
-var bodyParser = require('body-parser');
+const express = require('express')
+const path = require('path')
+const favicon = require('serve-favicon')
+const logger = require('morgan')
+const cookieParser = require('cookie-parser')
+const bodyParser = require('body-parser')
+const helmet = require('helmet')
+const session = require('express-session')
+const passport = require('passport')
 
-var routes = require('./routes/index');
-var users = require('./routes/users');
+const GitHubStrategy = require('passport-github2').Strategy
+const GITHUB_CLIENT_ID = '032bc86a651d91d9de8d'
+const GITHUB_CLIENT_SECRET = '8dbd1f516f7b65e4ee6855afe8952e6ec15b2468'
 
-var app = express();
+passport.serializeUser((user, done) => {
+  done(null, user)
+})
+
+passport.deserializeUser((obj, done) => {
+  done(null, obj)
+})
+
+passport.use(new GitHubStrategy(
+  {
+    clientID: GITHUB_CLIENT_ID,
+    clientSecret: GITHUB_CLIENT_SECRET,
+    callbackURL: 'http://localhost:8000/auth/github/callback'},
+  (accessToken, refleshToken, profile, done) => done(null, profile)
+))
+
+const routes = require('./routes/index')
+const login = require('./routes/login')
+const logout = require('./routes/logout')
+
+const app = express()
+app.use(helmet())
 
 // view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
+app.set('views', path.join(__dirname, 'views'))
+app.set('view engine', 'jade')
 
 // uncomment after placing your favicon in /public
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
-app.use(logger('dev'));
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false }));
-app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(logger('dev'))
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({ extended: false }))
+app.use(cookieParser())
+app.use(express.static(path.join(__dirname, 'public')))
 
-app.use('/', routes);
-app.use('/users', users);
+app.use(session({ secret: 'ad2e8a373c5ada68', resave: false, saveUninitialized: false }))
+app.use(passport.initialize())
+app.use(passport.session())
+
+app.use('/', routes)
+app.use('/login', login)
+app.use('/logout', logout)
+
+app.get('/auth/github',
+  passport.authenticate('github', { scope: ['user:email'] }),
+  (req, res) => {}
+)
+
+app.get('/auth/github/callback',
+  passport.authenticate('github', { failureRedirect: '/login' }),
+  (req, res) => {
+    res.redirect('/')
+  }
+)
 
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
-  var err = new Error('Not Found');
-  err.status = 404;
-  next(err);
-});
+app.use((req, res, next) => {
+  const err = new Error('Not Found')
+  err.status = 404
+  next(err)
+})
 
 // error handlers
 
 // development error handler
 // will print stacktrace
 if (app.get('env') === 'development') {
-  app.use(function(err, req, res, next) {
-    res.status(err.status || 500);
+  app.use((err, req, res, next) => {
+    res.status(err.status || 500)
     res.render('error', {
       message: err.message,
       error: err
-    });
-  });
+    })
+  })
 }
 
 // production error handler
 // no stacktraces leaked to user
-app.use(function(err, req, res, next) {
-  res.status(err.status || 500);
+app.use((err, req, res, next) => {
+  res.status(err.status || 500)
   res.render('error', {
     message: err.message,
     error: {}
-  });
-});
+  })
+})
 
 
-module.exports = app;
+module.exports = app
